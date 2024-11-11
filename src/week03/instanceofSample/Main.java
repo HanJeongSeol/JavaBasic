91742d6c5ed7cfedf568160136c0a634624926a0
HanJeongSeol <hanjeong94@naver.com>
2024-11-11 22:58:00 +0900

    3-15. 다형성과 instanceof

---
+++ src/week03/instanceofSample/Main.java
@@ -0,0 +1,29 @@
+package week03.instanceofSample;
+
+// 다형성
+
+class Parent { }
+class Child extends Parent { }
+class Brother extends Parent { }
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        // 부모 클래스로 형변환
+        Parent pc = new Child();  // 다형성 허용 (자식 -> 부모)
+
+        Parent p = new Parent();
+
+        System.out.println(p instanceof Object); // true 출력
+        System.out.println(p instanceof Parent); // true 출력
+        System.out.println(p instanceof Child);  // false 출력
+
+        Parent c = new Child();
+
+        System.out.println(c instanceof Object); // true 출력
+        System.out.println(c instanceof Parent); // true 출력
+        System.out.println(c instanceof Child);  // true 출력
+
+    }
+}
